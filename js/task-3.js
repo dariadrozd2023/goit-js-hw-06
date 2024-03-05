3c1bab18d22dfaced704392414e6dc5271456bc7
daria drozd <daria.drozd1990@gmail.com>
2024-03-05 10:37:33 +0200

    first

---
+++ js/task-3.js
@@ -0,0 +1,28 @@
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+
+  padStart(str) {
+    return (this.#value = str.concat(this.#value));
+  }
+  padEnd(str) {
+    return (this.#value = this.#value.concat(str));
+  }
+  padBoth(str) {
+    return (this.#value = str.concat(this.#value, str));
+  }
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
